328f978d31325b886aba649ebba7bd10b2e9462d
mikolajsolecki <157210875+123Mikolaj@users.noreply.github.com>
2024-02-19 00:49:30 +0100

    Add files via upload

---
+++ Project_PHP-MySQL/projekt/register_process.php
@@ -0,0 +1,93 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car Rental</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-toggleable-md">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Home</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="car_list.php">Nasze Samochody</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+<div class="main_content">    
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST["username"]);
+    $password = htmlspecialchars($_POST["password"]);
+    $confirm_password = htmlspecialchars($_POST["confirm_password"]);
+
+    // Validate password
+    if ($password !== $confirm_password) {
+        echo "Niezgodność Haseł";
+        exit();
+    }
+
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $conn = new mysqli('localhost', 'root', '', 'carrental');
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Check if the username is already taken
+    $check_username_query = "SELECT * FROM Users WHERE Username = ?";
+    $stmt = $conn->prepare($check_username_query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        echo "Nazwa Użytkownika zajęta, użyj innej nazwy";
+        $stmt->close();
+        $conn->close();
+        exit();
+    }
+
+    // Insert the new user into the database
+    $insert_user_query = "INSERT INTO Users (Username, PasswordHash, UserRole) VALUES (?, ?, 'user')";
+    $stmt = $conn->prepare($insert_user_query);
+    $stmt->bind_param("ss", $username, $hashed_password);
+
+    if ($stmt->execute()) {
+        echo "Rejestracja zakończona sukcesem. Możesz teraz się <a href='login.php'>zalogować</a>";
+    } else {
+        echo "Błąd podczas rejestracji: " . $stmt->error;
+        echo "<br><a href='index.php'>Powrót na stronę główną</a>";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
+</div>
+</body>
+</html>    
